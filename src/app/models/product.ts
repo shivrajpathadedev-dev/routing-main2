9e558f509ea790f4e41d56c7ad177d9a4e2ef649
shivrajpathadedev-dev <shivrajpathade.dev@gmail.com>
2026-06-28 11:19:38 +0530

    ok

---
+++ src/app/models/product.ts
@@ -0,0 +1,14 @@
+
+
+export interface Iproduct{
+     pname: string;
+    pid: string;
+    pstatus: string;
+    canReturn: number;
+    pimg: string;
+}
+
+export interface IresProduct<T>{
+    msg:string;
+    data:T
+}
